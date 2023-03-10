61917b11826cc9879bb76a7628b9643ccadb5255
Felipe Souza <felipe-_-br@hotmail.com>
2023-03-10 13:38:10 -0300

    Navegação SPA

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+const ID_PAGINAS = ['despesas','categorias'];
+
+
+const pageLoad = () => {
+    
+    const pageArray = ID_PAGINAS.map(el=>document.getElementById(el));
+    pageArray[0].classList.add('show');
+    for(let page of pageArray){
+        for(let node of document.querySelectorAll(`a[href="#${page.id}"]`)){
+            node.addEventListener('click',(e)=>{
+                e.preventDefault();
+                for(let pg of pageArray) pg.classList.remove('show');
+                page.classList.add('show');
+            })
+        }
+    }
+    
+}
+
+
+
+window.addEventListener('load',pageLoad)
